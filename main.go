e9bedb19ecdfefa8123f43551750a4644c0c0fdf
shreyash <shreyashgajlekar@gmail.com>
2020-11-26 16:41:56 +0530

    Changed encoder and decode to marshal and unmarshal for all http requests

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
@@ -12,7 +13,7 @@ import (
 
 //Student Model
 type Student struct {
-	ID      string `json: "id`
+	//ID      string `json: "id`
 	Class   string `json: "class"`
 	Section string `json: "section"`
 	Name    *Name  `json: "name"`
@@ -28,41 +29,94 @@ var students map[string]Student
 
 func getStudents(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.Marshal(students)
-
+	b, err := json.MarshalIndent(students, "", " ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(b)
 }
 
 func getStudent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "appication/json")
-	params := mux.Vars(r)
-
-	//Loop through books and find id
-	for _, item := range students {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
+	recID := mux.Vars(r)["id"]
+	s, ok := students[recID]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
 	}
-	json.NewEncoder(w).Encode(&Student{})
-
+	b, err := json.Marshal(s)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(b)
 }
 
 func createStudent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "appication/json")
-
+	b, err := ioutil.ReadAll(r.Body)
 	var student Student
-	_ = json.NewDecoder(r.Body).Decode(&student)
-	student.ID = strconv.Itoa(rand.Intn(1000))
-	students[student.ID] = student
-	json.NewEncoder(w).Encode(student)
+	error := json.Unmarshal(b, &student)
+	if error != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	id := strconv.Itoa(rand.Intn(2000))
+
+	students[id] = student
+
+	dataNew, err1 := json.MarshalIndent(students, "", " ")
+	if err1 != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(dataNew)
 }
 
 func updateStudent(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	recID := mux.Vars(r)["id"]
+	bData, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	var student Student
+	error := json.Unmarshal(bData, &student)
+	if error != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	students[recID] = student
 
+	dataNew, errMarshal := json.Marshal(students)
+	if errMarshal != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(dataNew)
 }
 
 func deleteStudent(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
+	recID := mux.Vars(r)["id"]
+
+	delete(students, recID)
+
+	b, err := json.Marshal(students)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(b)
 }
 
 func main() {
@@ -70,9 +124,9 @@ func main() {
 
 	students = make(map[string]Student)
 
-	students["1"] = Student{ID: "1", Class: "10th", Section: "A", Name: &Name{Firstname: "Akash", Lastname: "Ghate"}}
-	students["2"] = Student{ID: "2", Class: "10th", Section: "A", Name: &Name{Firstname: "Nik", Lastname: "Irving"}}
-	students["3"] = Student{ID: "3", Class: "10th", Section: "B", Name: &Name{Firstname: "Josh", Lastname: "Gates"}}
+	students["1"] = Student{Class: "10th", Section: "A", Name: &Name{Firstname: "Akash", Lastname: "Ghate"}}
+	students["2"] = Student{Class: "10th", Section: "A", Name: &Name{Firstname: "Nik", Lastname: "Irving"}}
+	students["3"] = Student{Class: "10th", Section: "B", Name: &Name{Firstname: "Josh", Lastname: "Gates"}}
 
 	router.HandleFunc("/api/students", getStudents).Methods("GET")
 	router.HandleFunc("/api/student/{id}", getStudent).Methods("GET")
