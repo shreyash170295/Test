b13fae17409c2884acc563eb55d137d6fd032312
shreyash <shreyashgajlekar@gmail.com>
2020-11-29 17:51:12 +0530

    Added proper error handling and fixed routes

--- main.go
+++ main.go
@@ -30,6 +30,9 @@ var students map[string]Student
 func getStudents(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	b, err := json.MarshalIndent(students, "", " ")
+	if b == nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -38,14 +41,26 @@ func getStudents(w http.ResponseWriter, r *http.Request) {
 }
 
 func getStudent(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.Write([]byte(`{"message":"incorrect Method expected get"}`))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 	w.Header().Set("Content-Type", "appication/json")
 	recID := mux.Vars(r)["id"]
+	if len(recID) == 0 {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
 	s, ok := students[recID]
 	if !ok {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 	b, err := json.Marshal(s)
+	if b == nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -67,11 +82,14 @@ func createStudent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	id := strconv.Itoa(rand.Intn(2000))
-
+	student.ID = id
 	students[id] = student
 
-	b, err1 := json.MarshalIndent(students, "", " ")
-	if err1 != nil {
+	b, err = json.MarshalIndent(students[id], "", " ")
+	if b == nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -82,20 +100,30 @@ func createStudent(w http.ResponseWriter, r *http.Request) {
 func updateStudent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	recID := mux.Vars(r)["id"]
+	if len(recID) == 0 {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
 	b, err := ioutil.ReadAll(r.Body)
+	if b == nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	var student Student
-	error := json.Unmarshal(b, &student)
-	if error != nil {
+	err = json.Unmarshal(b, &student)
+	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	students[recID] = student
 
 	b, err = json.Marshal(students)
+	if b == nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -108,10 +136,17 @@ func deleteStudent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	recID := mux.Vars(r)["id"]
+	if len(recID) == 0 {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
 
 	delete(students, recID)
 
 	b, err := json.Marshal(students)
+	if b == nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -128,11 +163,11 @@ func main() {
 	students["2"] = Student{ID: "2", Class: "10th", Section: "A", Name: &Name{Firstname: "Nik", Lastname: "Irving"}}
 	students["3"] = Student{ID: "3", Class: "10th", Section: "B", Name: &Name{Firstname: "Josh", Lastname: "Gates"}}
 
-	router.HandleFunc("/api/students", getStudents).Methods("GET")
-	router.HandleFunc("/api/student/{id}", getStudent).Methods("GET")
-	router.HandleFunc("/api/student", createStudent).Methods("POST")
-	router.HandleFunc("/api/student/{id}", updateStudent).Methods("PUT")
-	router.HandleFunc("/api/student/{id}", deleteStudent).Methods("DELETE")
+	router.HandleFunc("/api/students", getStudents).Methods("GET").Name("GetAllStudents")
+	router.HandleFunc("/api/students/{id}", getStudent).Methods("GET").Name("GetStudentByID")
+	router.HandleFunc("/api/students", createStudent).Methods("POST").Name("AddStudentByID")
+	router.HandleFunc("/api/students/{id}", updateStudent).Methods("PUT").Name("UpdateStudentByID")
+	router.HandleFunc("/api/students/{id}", deleteStudent).Methods("DELETE").Name("DeleteStudentByID")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 
